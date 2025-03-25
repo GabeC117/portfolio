f2f90ada35b59a5e57cec76333412123dda111be
Gabe <gabe.cespedes11@gmail.com>
2025-03-25 17:12:01 -0500

    Updates

---
+++ precache-manifest.e46c59df629d85fe85fa73cd7959d052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331ee6f29fca6942551847c623efe7e9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d3c2b58faf1eba326bc4",
+    "url": "/portfolio/static/css/main.dd6ae68f.chunk.css"
+  },
+  {
+    "revision": "0acf71de32388da24335",
+    "url": "/portfolio/static/js/2.00514b13.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/portfolio/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "d3c2b58faf1eba326bc4",
+    "url": "/portfolio/static/js/main.6b9751c5.chunk.js"
+  },
+  {
+    "revision": "e2099927ed85d6b29c73",
+    "url": "/portfolio/static/js/runtime~main.79032ead.js"
+  }
+]);

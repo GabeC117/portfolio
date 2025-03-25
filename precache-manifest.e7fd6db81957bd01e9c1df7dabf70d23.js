9f41f3f9fb563176561960b74fdd1dff4d02974a
Gabe <gabe.cespedes11@gmail.com>
2025-03-25 17:15:27 -0500

    Updates

--- precache-manifest.e7fd6db81957bd01e9c1df7dabf70d23.js
+++ precache-manifest.e7fd6db81957bd01e9c1df7dabf70d23.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "331ee6f29fca6942551847c623efe7e9",
+    "revision": "c6ce61bdd14177f8f30bec58f5445e43",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "d3c2b58faf1eba326bc4",
+    "revision": "57a5f26fec9ba9880aa6",
     "url": "/portfolio/static/css/main.dd6ae68f.chunk.css"
   },
   {
-    "revision": "0acf71de32388da24335",
-    "url": "/portfolio/static/js/2.00514b13.chunk.js"
+    "revision": "5267ad1e5c50fbd39131",
+    "url": "/portfolio/static/js/2.05681f65.chunk.js"
   },
   {
     "revision": "c87ae3f2a437a1b2df8d",
     "url": "/portfolio/static/js/3.177faaa1.chunk.js"
   },
   {
-    "revision": "d3c2b58faf1eba326bc4",
-    "url": "/portfolio/static/js/main.6b9751c5.chunk.js"
+    "revision": "57a5f26fec9ba9880aa6",
+    "url": "/portfolio/static/js/main.53e55675.chunk.js"
   },
   {
     "revision": "e2099927ed85d6b29c73",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e46c59df629d85fe85fa73cd7959d052.js"
+  "/portfolio/precache-manifest.e7fd6db81957bd01e9c1df7dabf70d23.js"
 );
 
 self.addEventListener('message', (event) => {
